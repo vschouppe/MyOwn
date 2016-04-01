f688bd110e0d430ba071d76ba841e7e62922e815
Aimee <Aimee@Aimee_Laptop>
2016-04-01 17:38:16 +1300

    remove test

--- messenger/src/main/java/org/vs/resttraining/messenger/MyResource.java
+++ messenger/src/main/java/org/vs/resttraining/messenger/MyResource.java
@@ -14,7 +14,7 @@ public class MyResource {
     /**
      * Method handling HTTP GET requests. The returned object will be sent
      * to the client as "text/plain" media type.
-     * test
+     * 
      * @return String that will be returned as a text/plain response.
      */
     @GET
