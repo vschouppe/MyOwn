92174922b057f06b6fe444a20935f96dd3b1a203
Aimee <Aimee@Aimee_Laptop>
2016-04-10 14:18:57 +1200

    rest ws 21

--- messenger/src/main/java/org/vs/resttraining/messenger/resources/MessageResource.java
+++ messenger/src/main/java/org/vs/resttraining/messenger/resources/MessageResource.java
@@ -2,7 +2,9 @@ package org.vs.resttraining.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -15,23 +17,25 @@ import org.vs.resttraining.messenger.service.MessageService;
 public class MessageResource {
 
 	MessageService ms = new MessageService();
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
 	public List<Message> getMessages() {
 		return this.ms.getAllMessages();
 	}
-	
-	
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message) {
+		return ms.addMessage(message);
+	}
 
 	@GET
 	@Path("/{messageId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Message getMessage(@PathParam ("messageId") long messageId) {
+	public Message getMessage(@PathParam("messageId") long messageId) {
 		return ms.getMessage(messageId);
 	}
-	
-	
-	
-	
+
 }
